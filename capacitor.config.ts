5c71800ac3a4240c153b0d56e95f3211fff92be1
emmanuelphoenix09 <141434765+emmanuelphoenix09@users.noreply.github.com>
2026-09-15 00:45:06 +0100

    Change webDir from 'www' to 'dist'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.phoenixart.geglise',
   appName: 'G-Église',
-  webDir: 'www',
+  webDir: "dist" ,
 };
 
 export default config;
