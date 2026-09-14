6315b4e701f0b16495392b4adbce32ec0acfa0f5
emmanuelphoenix09 <141434765+emmanuelphoenix09@users.noreply.github.com>
2026-09-14 23:27:38 +0100

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.phoenixart.geglise',
+  appName: 'G-Eglise',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
