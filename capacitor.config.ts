70425873822e0ff15c4a05ddcf089822769d288f
emmanuelphoenix09 <141434765+emmanuelphoenix09@users.noreply.github.com>
2026-09-15 08:15:38 +0100

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.phoenixart.geglise',
-  appName: 'G-Église',
-  webDir: "dist" ,
-};
-
-export default config;
